27b262fa67e7665da7e8e626c0b9b67eb0e0ac91
Patrik Evertsson <patrik.evertsson@decisio.se>
2019-11-24 15:36:24 +0100

    misc

---
+++ TrainTraffic/Views/Train45_2017.sql
@@ -0,0 +1,34 @@
+﻿CREATE VIEW [dbo].[Train45_2017]
+	AS  SELECT 
+      cast([departureDate] as date) as DepartureDate
+      ,[runningCurrently]
+      ,case timeTableRowActualTime  
+         when '' then null 
+        else cast([timeTableRowActualTime] as [datetime]) 
+        end as ActualTime
+      ,[timeTableRowCancelled] as Cancelled
+      ,[timeTableRowCommercialStop] as CommercialStop
+      ,[timeTableRowCommercialTrack] as CommercialTrack
+      ,[timeTableRowDifferenceInMinutes] MinuteDifference
+      ,[timeTableRowEstimateSource] as EstimateSource
+      ,case timeTableRowLiveEstimateTime 
+        when '' then null
+        else cast([timeTableRowLiveEstimateTime] as [datetime]) end as timeTableRowLiveEstimateTime
+      ,cast([timeTableRowScheduledTime] as [datetime]) as ScheduledTime
+      ,[timeTableRowStationShortCode] as StationCode
+      ,[timeTableRowTrainReadyAccepted] as TrainReadyAccepted 
+      ,case timeTableRowTrainReadyTimestamp 
+        when '' then null
+        else cast([timeTableRowTrainReadyTimestamp] as [datetime]) 
+        end as TrainReadyTime
+      ,[timeTableRowTrainStopping] as TrainStopping
+      ,[timeTableRowType] as DepatureArrival
+      ,[timetableAcceptanceDate] AcceptanceDate
+      ,[timetableType] 
+      ,[trainCategory] TrainCategory
+      ,[trainNumber] as TrainNumber 
+      ,[trainType] TrainType
+  FROM land.train45
+  
+
+
